095cd1790fe389b63d098585e3f55c00a2b550ac
Douglas <toddynotes@gmail.com>
2023-01-17 00:18:05 -0300

    feat: aula 1 - fundamentos do nest.js e prisma

---
+++ src/create-notification-body.ts
@@ -0,0 +1,15 @@
+import { IsNotEmpty, IsUUID, Length } from 'class-validator';
+
+export class CreateNotificationBody {
+
+  @IsNotEmpty()
+  @IsUUID()
+  recipientId: string;
+
+  @Length(5, 240)
+  content: string;
+
+  @IsNotEmpty()
+  category: string;
+
+}
